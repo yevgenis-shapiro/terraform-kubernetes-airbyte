7bbcb0111e50cffaf56805cd2df74a7e81425789
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-16 21:46:47 +0300

    update

---
+++ variable.tf
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+variable "kind_cluster_name" {
+  description = "The name of the Kind cluster"
+  type        = string
+}
+
+variable "kind_cluster_config_path" {
+  description = "Path to save the Kind kubeconfig file"
+  type        = string
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version to use for Kind nodes"
+  type        = string
+}
+
+variable "worker_count" {
+  description = "Number of worker nodes"
+  type        = number
+}
+
+variable "additional_control_planes_count" {
+  description = "Number of additional control planes for HA"
+  type        = number
+  default     = 2
+}
