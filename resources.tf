a374f8a6d8c6e43d922861a99dd35ce09e0a3174
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-16 21:46:01 +0300

    update

---
+++ resources.tf
@@ -0,0 +1,31 @@
+
+
+data "external" "subnet" {
+  program = ["/bin/bash", "-c", "docker network inspect -f '{{json .IPAM.Config}}' kind | jq .[0]"]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand(var.kind_cluster_config_path)
+  }
+}
+
+module "metallb" {
+  source = "./modules/metallb"
+  depends_on = [kind_cluster.default]
+  kind_cluster_config_path = var.kind_cluster_config_path
+}
+
+module "nginx" {
+  source = "./modules/nginx"
+  depends_on = [module.metallb]
+}
+
+module "airbyte" {
+  source = "./modules/airbyte"
+  depends_on = [module.nginx]
+}
+
